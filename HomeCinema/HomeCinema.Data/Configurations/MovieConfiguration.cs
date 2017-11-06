937f3e64d57e55a8c252208e207d4cdcf4c99585
gangapokhrel <ganga.pkhrel@gmail.com>
2017-11-06 15:05:47 -0500

    work

---
+++ HomeCinema/HomeCinema.Data/Configurations/MovieConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeCinema.Entities;
+namespace HomeCinema.Data.Configurations
+{
+    class MovieConfiguration:EntityBaseConfiguration<Movie>
+    {
+        public MovieConfiguration()
+        {
+            Property(m => m.Title).IsRequired().HasMaxLength(100);
+            Property(m => m.GenreId).IsRequired();
+            Property(m => m.Director).IsRequired().HasMaxLength(100);
+            Property(m => m.Writer).IsRequired().HasMaxLength(50);
+            Property(m => m.Producer).IsRequired().HasMaxLength(50);
+            Property(m => m.Writer).HasMaxLength(50);
+            Property(m => m.Producer).HasMaxLength(50);
+            Property(m => m.Rating).IsRequired();
+            Property(m => m.Description).IsRequired().HasMaxLength(2000);
+            Property(m => m.TrailerURI).HasMaxLength(200);
+            HasMany(m => m.Stocks).WithRequired().HasForeignKey(s => s.MovieId);
+        }
+
+        private bool HasMany(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object Property(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ HomeCinema/HomeCinema.Web/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,64 @@
+﻿using Autofac;
+using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Services;
+using HomeCinema.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace HomeCinema.Web.App_Start
+{
+    public class AutofacWebapiConfig
+    {
+        public static IContainer Container;
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // EF HomeCinemaContext
+            builder.RegisterType<Data.HomeCinemaContext>()
+                   .As<DbContext>()
+                   .InstancePerRequest();
+
+            builder.RegisterType<DbFactory>()
+                .As<IDbFactory>()
+                .InstancePerRequest();
+
+            builder.RegisterType<UnitOfWork>()
+                .As<Data.Infrastructure.IUnitOfWork>()
+                .InstancePerRequest();
+
+            builder.RegisterGeneric(typeof(Data.Repositories.EntityBaseRepository<>))
+                   .As(typeof(IEntityBaseRepository<>))
+                   .InstancePerRequest();
+
+            // Services
+            builder.RegisterType<Services.EncryptionService>()
+                .As<IEncryptionService>()
+                .InstancePerRequest();
+
+            builder.RegisterType<MembershipService>()
+                .As<IMembershipService>()
+                .InstancePerRequest();
+
+            Container = builder.Build();
+
+            return Container;
+        }
+    }
+}
